05be7025b20ec62c56f40c26314f61b2635adc6c
Maxc040 <Max.cornelissen@student.fontys.nl>
2023-03-16 11:21:27 +0100

    Alarm Opdracht

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// Controleer of er een argument is meegegeven
+	if len(os.Args) == 1 {
+		fmt.Println("Geef mij nummer a sabbi.")
+		return
+	}
+
+	// Zet het argument om naar een integer
+	n, err := strconv.Atoi(os.Args[1])
+
+	// Controleer of er een error is opgetreden tijdens de conversie of als n <= 0 is
+	if err != nil || n <= 0 {
+		fmt.Println("Wallah jij probeert fraude te plegen.")
+		return
+	}
+
+	// Herhaal de tekst "Alarm x !" n keer, waarbij x ophoogt bij elke iteratie
+	for i := 1; i <= n; i++ {
+		fmt.Printf("Politie %d !\n", i)
+	}
+}
