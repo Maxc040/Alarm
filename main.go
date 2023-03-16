347020c313dd21bbd2c8330979b0ce05fb47388d
Maxc040 <Max.cornelissen@student.fontys.nl>
2023-03-16 15:35:08 +0100

    Versie 2 Alarm stopt nu bij 10 door loop

--- main.go
+++ main.go
@@ -19,11 +19,14 @@ func main() {
 	// Controleer of er een error is opgetreden tijdens de conversie of als n <= 0 is
 	if err != nil || n <= 0 {
 		fmt.Println("Wallah jij probeert fraude te plegen.")
-		return
 	}
 
 	// Herhaal de tekst "Alarm x !" n keer, waarbij x ophoogt bij elke iteratie
 	for i := 1; i <= n; i++ {
 		fmt.Printf("Politie %d !\n", i)
+		if i >= 10 {
+			fmt.Println("Wallah jij probeert fraude te plegen.")
+			break
+		}
 	}
 }
