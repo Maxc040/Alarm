e30907f0c31c7915fa98e1b2d1089ec2c42f0f86
Maxc040 <Max.cornelissen@student.fontys.nl>
2023-03-17 13:33:38 +0100

    1.3 Alarm

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	// Controleer of er een argument is meegegeven
 	if len(os.Args) == 1 {
-		fmt.Println("Geef mij nummer a sabbi.")
+		fmt.Println("Geef mij nummer.")
 		return
 	}
 
@@ -18,14 +18,14 @@ func main() {
 
 	// Controleer of er een error is opgetreden tijdens de conversie of als n <= 0 is
 	if err != nil || n <= 0 {
-		fmt.Println("Wallah jij probeert fraude te plegen.")
+		fmt.Println("Wallah jij probeert weg te rennen.")
 	}
 
-	// Herhaal de tekst "Alarm x !" n keer, waarbij x ophoogt bij elke iteratie
+	// Herhaal de tekst "Politie x !" n keer, waarbij x ophoogt bij elke iteratie
 	for i := 1; i <= n; i++ {
 		fmt.Printf("Politie %d !\n", i)
 		if i >= 10 {
-			fmt.Println("Wallah jij probeert fraude te plegen.")
+			fmt.Println("Wallah jij probeert weg te rennen.") //wanneer de input niet klopt, dus het is onder de 1 of boven de 10, dan krijgt de gebruiker een eroor message.
 			break
 		}
 	}
